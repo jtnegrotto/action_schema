582b305f7d2dd9ce6c1dd6115afe06dd127dd75a
Julien Negrotto <jtnegrotto@gmail.com>
2024-12-08 13:25:16 -0600

    Create gem skeleton

---
+++ lib/action_schema.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "action_schema/version"
+
+module ActionSchema
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/action_schema/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ActionSchema
+  VERSION = "0.1.0"
+end
